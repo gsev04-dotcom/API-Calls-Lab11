b65620aaf6209b0dd05447a3d6845b955bacd23b
Gretle Severin-Augustin <gsev04@gmail.com>
2026-08-06 22:09:01 -0400

    Handle XHR response and display info

--- script.js
+++ script.js
@@ -22,6 +22,18 @@ document.getElementById('fetchBtn').addEventListener('click', function() {
         const xhr = new XMLHttpRequest();
         xhr.open (GET, 'https://jsonplaceholder.typicode.com/posts/2', true);
          
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                const post =JSON.parse(xhr.responseText);
+                
+                document.getElementById('display').innerHTML = `<h3>${post.title}</h3> <p>${post.body}</p>`;
+
+            }else{
+                document.getElementById('display').innerHTML = `<span class="error> Error: ${xhr.statusText}</span>`;
+
+            };
+            
+        }
     
 
 
